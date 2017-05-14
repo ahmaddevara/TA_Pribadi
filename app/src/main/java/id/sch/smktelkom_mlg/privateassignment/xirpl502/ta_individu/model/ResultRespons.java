fba0aa853182818a6f8e9d2ea6c38f5be06d591a
ahmaddevara <devaraahmad@gmail.com>
2017-05-14 22:30:55 +0700

    commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl502/ta_individu/model/ResultRespons.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl502.ta_individu.model;
+
+import java.util.List;
+
+public class ResultsRespons {
+    public String page;
+    public List<Results> results;
+}
